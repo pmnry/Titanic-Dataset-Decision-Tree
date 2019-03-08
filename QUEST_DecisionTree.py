dcc77547da6333ec81b2d6b7779b611212bafe34
Pierre Monroy <43237869+pmnry@users.noreply.github.com>
2019-03-08 18:48:12 +0000

    Add files via upload
    Adding features engineering and XGBoost model

--- QUEST_DecisionTree.py
+++ QUEST_DecisionTree.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.decomposition import PCA
@@ -10,6 +12,7 @@ from sklearn.tree import export_graphviz
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import re
 
 
 # Kaggle Titanic Dataset
@@ -17,16 +20,46 @@ import matplotlib.pyplot as plt
 # We will first run some data analysis and data visualization and then implement the model and analyze its results.
 
 df_train = pd.read_csv('train.csv')
-# df_train.dropna(inplace=True)
 
 df_test = pd.read_csv('test.csv')
-# df_test.dropna(inplace=True)
+
+fulldataset = [df_train, df_test]
+
+# Creating the name length variable
+for df in fulldataset:
+    df['NameLength'] = df['Name'].apply(len)
+
+# Creating the family size variable
+for df in fulldataset:
+    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+
+# Creating the isAlone variable
+for df in fulldataset:
+    df['isAlone'] = df['FamilySize'].apply(lambda x: 1 if x==1 else 0)
+
+# Define function to extract titles from passenger names
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+# Create a new feature Title, containing the titles of passenger names
+for dataset in fulldataset:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+
+for dataset in fulldataset:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
 NumImp = SimpleImputer(missing_values=np.nan, strategy='median')
 NumCol = ['Age', 'Fare']
 
 CatImp = SimpleImputer(strategy='constant')
-CatCol = ['Embarked', 'Sex', 'Pclass']
+CatCol = ['Embarked', 'Sex', 'Pclass', 'Title']
 
 for col in NumCol:
     df_train[col] = NumImp.fit_transform(df_train[col].values.reshape(-1,1))
@@ -62,7 +95,7 @@ plt.show()
 ### Creating discrete variables out of categorical data
 # If we want to be able to use variables such as 'Embarked' in our classifier we need to modify these in order to turn them into discrete variable. To do that we use the `LabelEncoder` which is part of the `sklearn` package.
 
-categorical_feats = ['Embarked', 'Sex']
+categorical_feats = ['Embarked', 'Sex', 'Title']
 
 for feat in categorical_feats:
     le = preprocessing.LabelEncoder()
@@ -72,14 +105,13 @@ for feat in categorical_feats:
 
 ### Implementing a PCA decomposition
 
-features = ['Fare', 'Embarked', 'Sex', 'Age']
+features = ['Fare', 'Embarked', 'Sex', 'Age', 'isAlone', 'FamilySize', 'NameLength', 'Title']
 pca = PCA(n_components=4)
 pca.fit(df_train[features])
 print(pca.explained_variance_ratio_)
 
 ### Fitting a simple Decision Tree
 
-features = ['Fare', 'Embarked', 'Sex', 'Age']
 model = DecisionTreeClassifier()
 fit_model = model.fit(df_train[features], df_train['Survived'])
 y_pred = fit_model.predict(df_train[features])
@@ -88,7 +120,13 @@ y_pred = fit_model.predict(df_train[features])
 ### Displaying its accuracy
 
 # Model Accuracy, how often is the classifier correct?
-print("Accuracy:", metrics.accuracy_score(df_train['Survived'], y_pred))
+print("Accuracy Simple Decision Tree:", metrics.accuracy_score(df_train['Survived'], y_pred))
+
+### Prediction on test set
+
+y_pred_tree_test = fit_model.predict(df_test[features])
+df_results_tree = pd.DataFrame(y_pred_tree_test, index=df_test['PassengerId'], columns=['Survived'])
+df_results_tree.to_csv('results_tree.csv')
 
 ### Testing Random Forest with hyper parameters tuning
 
@@ -116,9 +154,8 @@ random_grid = {'n_estimators': n_estimators,
 
 ### Fitting a simple Random Forest
 
-features = ['Fare', 'Embarked', 'Sex', 'Age']
 model_RF = RandomForestClassifier()
-model_RF_random = RandomizedSearchCV(estimator=model_RF, param_distributions=random_grid, n_iter=100, cv=3, verbose=2,
+model_RF_random = RandomizedSearchCV(estimator=model_RF, param_distributions=random_grid, n_iter=100, cv=3, verbose=0,
                                      random_state=42, n_jobs=-1)
 model_RF_random.fit(df_train[features], df_train['Survived'])
 y_pred_RF = model_RF_random.predict(df_train[features])
@@ -126,10 +163,91 @@ y_pred_RF = model_RF_random.predict(df_train[features])
 ### Displaying its accuracy
 
 # Model Accuracy, how often is the classifier correct?
-print("Accuracy:", metrics.accuracy_score(df_train['Survived'], y_pred_RF))
+print("Accuracy Random Forest:", metrics.accuracy_score(df_train['Survived'], y_pred_RF))
 
 ### Prediction on test set
 
 y_pred_RF_test = model_RF_random.predict(df_test[features])
 df_results = pd.DataFrame(y_pred_RF_test, index=df_test['PassengerId'], columns=['Survived'])
-df_results.to_csv('results.csv')
+df_results.to_csv('results_RF.csv')
+
+### Testing SVM with hyper parameters tuning
+
+# Penalization parameter
+C = [float(x) for x in np.linspace(start = 0.01, stop = 2, num = 100)]
+# Kernel applied on data
+kernel = ['linear', 'rbf', 'sigmoid']
+# Kernel coefficient
+# gamma = [float(x) for x in np.linspace(0.01, 10, num = 100)]
+
+# Create the random grid
+random_grid_SVM = {'C': C,
+               'kernel': kernel,
+               'gamma':['auto']}
+
+### Fitting a simple SVM
+
+
+model_SVM = SVC()
+model_SVM_random = RandomizedSearchCV(estimator=model_SVM, param_distributions=random_grid_SVM, n_iter=100, cv=3, verbose=0,
+                                     random_state=42, n_jobs=-1)
+model_SVM_random.fit(df_train[features], df_train['Survived'])
+y_pred_SVM = model_SVM_random.predict(df_train[features])
+
+### Displaying its accuracy
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy SVM:", metrics.accuracy_score(df_train['Survived'], y_pred_SVM))
+
+### Prediction on test set
+
+y_pred_SVM_test = model_SVM_random.predict(df_test[features])
+df_results_SVM = pd.DataFrame(y_pred_SVM_test, index=df_test['PassengerId'], columns=['Survived'])
+df_results_SVM.to_csv('results_SVM.csv')
+
+### Testing Gradient Boosting Classifier with hyper parameters tuning
+
+# Loss function to be optimized
+loss = ['deviance', 'exponential']
+# Learning rate
+learning_rate = [float(x) for x in np.linspace(0.01, 1, num = 20)]
+# Number of boosting stages
+n_estimators = [int(x) for x in np.linspace(10, 200, num = 100)]
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+
+# Create the random grid
+random_grid_XGB = {'loss': loss,
+               'learning_rate': learning_rate,
+               'n_estimators': n_estimators,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'max_features': max_features,
+               'max_depth': max_depth}
+
+### Fitting a simple SVM
+
+
+model_XGB = GradientBoostingClassifier()
+model_XGB_random = RandomizedSearchCV(estimator=model_XGB, param_distributions=random_grid_XGB, n_iter=100, cv=3, verbose=0,
+                                     random_state=42, n_jobs=-1)
+model_XGB_random.fit(df_train[features], df_train['Survived'])
+y_pred_XGB = model_XGB_random.predict(df_train[features])
+
+### Displaying its accuracy
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy XGB:", metrics.accuracy_score(df_train['Survived'], y_pred_XGB))
+
+### Prediction on test set
+
+y_pred_XGB_test = model_XGB_random.predict(df_test[features])
+df_results_XGB = pd.DataFrame(y_pred_XGB_test, index=df_test['PassengerId'], columns=['Survived'])
+df_results_XGB.to_csv('results_XGB.csv')
